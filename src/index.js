7aad853683298d7616f2540df28db28fee60ddc1
Mohan-Villuri-Divami <sivasankar@divami.com>
2024-04-26 14:43:14 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import MuiSearch from "./components/MuiSearch";
+import MuiSearch from "./components/fetch/MuiSearch";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
